5ce55e4a1faee2e0e2d49690ef71e1595c66f627
Amar Baldewsing <088681@glr.nl>
2024-03-05 13:50:13 +0100

    upload limiet

--- src/db/upload.php
+++ src/db/upload.php
@@ -1,31 +1,33 @@
 <?php 
-// Include the database configuration file 
+
 include_once '../conf/dbconn.php'; 
  
 $statusMsg = ''; 
  
-// File upload directory 
+
 $targetDir = "../foto upload/uploads/"; 
 
 if(isset($_POST["submit"])){ 
     if(!empty($_FILES["file"]["name"])){ 
-        $fileName = basename($_FILES["file"]["name"]);   
+        if ($_FILES["file"]["size"] <= 20000000) {
+        $fileName = basename($_FILES["file"]["name"]); 
+        $fileName =  $fileName;        
         $targetFilePath = $targetDir . $fileName; 
         $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
         
-        // Get description from POST data
+        
         $beschrijving = $_POST["description"];
      
-        // Allow certain file formats 
+        
         $allowTypes = array('jpg','png','jpeg','gif'); 
         if(in_array($fileType, $allowTypes)){ 
-            // Upload file to server 
+            
             if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){ 
-                // Insert image file name and description into database 
+                
                 $insert = $db->query("INSERT INTO images (file_name, uploaded_on, beschrijving) VALUES ('".$fileName."', NOW(), '".$beschrijving."')"); 
                 if($insert){ 
                     $statusMsg = "The file ".$fileName. " has been uploaded successfully."; 
-                    // Redirect to upload_success.php after successful upload
+                    
                     header("Location: ../db/upload_success.php");
                     exit();
                 }else{ 
@@ -40,8 +42,11 @@ if(isset($_POST["submit"])){
     }else{ 
         $statusMsg = 'Please select a file to upload.'; 
     } 
+    }else{
+        $statusMsg = 'Sorry, your file is too large.';
+    }
 } 
  
-// Display status message 
+
 echo $statusMsg; 
 ?>
