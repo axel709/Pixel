da3676690a78a3cdac8ad6313dd066f8ad7ac36a
Axel <axel@v-toor.nl>
2024-03-05 13:47:02 +0100

    filename

--- src/db/upload.php
+++ src/db/upload.php
@@ -9,8 +9,7 @@ $targetDir = "../foto upload/uploads/";
 
 if(isset($_POST["submit"])){ 
     if(!empty($_FILES["file"]["name"])){ 
-        $fileName = basename($_FILES["file"]["name"]); 
-        $fileName =  $fileName;        
+        $fileName = basename($_FILES["file"]["name"]);   
         $targetFilePath = $targetDir . $fileName; 
         $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
         
